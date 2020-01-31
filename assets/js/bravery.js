299008b866d325328241dfdf14d0d3e009808824
Joel Goodman <joel@braverymedia.co>
2020-01-31 11:10:16 -0600

    Add lazyload images

--- assets/js/bravery.js
+++ assets/js/bravery.js
@@ -33,3 +33,27 @@ if (closeContact) {
     }
   })
 }
+
+// Lazy loading images
+const images = document.querySelectorAll('[data-src]');
+const config = {
+  rootMargin: '0px 0px 50px 0px',
+  threshold: 0
+};
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        preloadImage(entry.target);
+        self.unobserve(entry.target);
+      }
+  });
+}, config);
+
+images.forEach(image => { observer.observe(image); });
+
+function preloadImage(img) {
+  const src = img.getAttribute('data-src');
+  if (!src) { return; }
+  img.src = src;
+  img.classList.add('lazy-loaded');
+}
